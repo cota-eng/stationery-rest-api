98c9fdc3f4e71490c8952f8555cef636ac8cdeb4
cota <65804288+cota-eng@users.noreply.github.com>
2021-01-28 17:18:58 +0900

    category

--- app/pen/models.py
+++ app/pen/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+    """Model that define category and id is normal"""
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
