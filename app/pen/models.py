2e598e79968a38925b32729a529eef8b6f771817
cota <65804288+cota-eng@users.noreply.github.com>
2021-01-28 20:22:25 +0900

    add tests

--- app/pen/models.py
+++ app/pen/models.py
@@ -2,25 +2,29 @@ from django.db import models
 import uuid
 from django.utils.translation import gettext as _
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.conf import settings
+from django.utils import timezone
 
 class Category(models.Model):
     """Model that define category and id is normal"""
-    name = models.CharField(_("category name"),max_length=50)
+    name = models.CharField(_("category name"),max_length=20)
     def __str__(self):
         return self.name
 
 class Productor(models.Model):
+    """Model that define Maker and has name"""
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
+    """Model that has diffrent tags, such as Wood, Light, Rich ..."""
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
 class Pen(models.Model):
+    """Model that is main part"""
     name = models.CharField(_("pen's name"), max_length=50)
     description = models.TextField()
     category = models.ForeignKey(
@@ -42,4 +46,7 @@ class Pen(models.Model):
         related_name="pen_tag",
         on_delete=models.CASCADE
     )
-    image=models.ImageField(_("pen's images"), upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(_("pen's images"), upload_to=None, height_field=None, width_field=None, max_length=None)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
