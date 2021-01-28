56e943960451c02e737f2211011220b377dc4b06
cota <65804288+cota-eng@users.noreply.github.com>
2021-01-28 20:12:10 +0900

    add productor tag model

--- app/pen/models.py
+++ app/pen/models.py
@@ -10,16 +10,36 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Productor(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Pen(models.Model):
     name = models.CharField(_("pen's name"), max_length=50)
+    description = models.TextField()
     category = models.ForeignKey(
         Category,
         related_name="pen_category",
         on_delete=models.CASCADE
         )
-    price_yen = models.PositiveIntegerField(_("price"),validater=[MaxValueValidator(100000),])
-    description = models.TextField()
-    # shop
-    # productor
+    price_yen = models.PositiveIntegerField(
+        _("price"),
+        validater=[MaxValueValidator(100000),]
+        )
+    productor = models.ForeignKey(
+        Productor,
+        related_name="pen_productor",
+        on_delete=models.CASCADE
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        related_name="pen_tag",
+        on_delete=models.CASCADE
+    )
     image=models.ImageField(_("pen's images"), upload_to=None, height_field=None, width_field=None, max_length=None)
