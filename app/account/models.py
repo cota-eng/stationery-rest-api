f8a7836de732609c3ab1ca1cdd227f4f0993a978
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-07 16:02:22 +0900

    add some changes

--- app/account/models.py
+++ app/account/models.py
@@ -4,6 +4,10 @@ from django.conf import settings
 import uuid
 import os
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.translation import gettext as _
+
+
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
     def _create_user(self, email, password, **extra_fields):
@@ -39,9 +43,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     Custom user model that supports using email instead of username
     and using uuid4
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
-    email = models.EmailField(max_length=255, unique=True)
-    is_verified = models.BooleanField(default=False)
+    id = models.UUIDField(
+        _('uuid'),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True)
+    email = models.EmailField(_('email'),max_length=255, unique=True)
+    is_verified = models.BooleanField(_('verified'),default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     auth_provider = models.CharField(
@@ -72,14 +81,16 @@ class Profile(models.Model):
     """Model that has avatar and dates of create and update"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
     user_profile = models.OneToOneField(
+        _('user'),
         User,
         related_name="user_profile",
         on_delete=models.CASCADE
         )
-    nickname = models.CharField(max_length=50,default="匿名ユーザー")
+    nickname = models.CharField(_('nickname'),max_length=50,default="匿名ユーザー")
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now_add=True)
     avatar = models.ImageField(upload_to=profile_avatar_path, height_field=None, width_field=None, max_length=None)
+    twitter_account = models.CharField(_('twitter username'),null=True,blank=True)
     # favorite_pen = models.ManyToManyField()
     def __str__(self):
-        return f'Profile:{self.nickname}'
+        return f'Profile of {self.nickname}'
