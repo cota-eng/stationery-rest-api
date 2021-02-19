5bd01e9390b56d73822b3f575ee2407743bc922d
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-19 22:49:55 +0900

    add

--- app/authentication/permissions.py
+++ app/authentication/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import generics, mixins, permissions
 
 class UserIsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    IsAuthenticated is user can see info about profile
+    personA can change PersonB profile
+    this is not allowed, so this permission is needed
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
