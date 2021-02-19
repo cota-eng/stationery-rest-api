bf73eb001a72c744f019129b834bf0fe1b9ef29b
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-19 23:01:52 +0900

    fix:id missed

--- app/authentication/permissions.py
+++ app/authentication/permissions.py
@@ -9,4 +9,4 @@ class UserIsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.id == request.user.id
+        return obj.id == request.user.user_profile.id
