bfb7d44a008ea4cbcc13a12b81aaff9e284e6067
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-20 11:57:44 +0900

    add: media

--- app/config/urls.py
+++ app/config/urls.py
@@ -27,3 +27,14 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# import debug_toolbar
+# if settings.DEBUG:
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ] 
