c9c752f4646df93e3aeca751555e9f0a67161411
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-23 17:01:41 +0900

    settin

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -8,11 +8,11 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 ALLOWED_HOSTS = [""]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-from django.conf import settings
-BASE_DIR = settings.BASE_DIR
+# from django.conf import settings
+# BASE_DIR = settings.BASE_DIR
 import django_heroku
 django_heroku.settings(locals())
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
