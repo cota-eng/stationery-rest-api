2d838ddb7c6394de0887b41a60cc43ebfe8c8b84
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-23 18:30:52 +0900

    some change

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -80,13 +80,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE=True
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_PRELOAD=True
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
@@ -296,7 +290,6 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-
 
 REST_USE_JWT = True
 # JWT_AUTH_COOKIE = 'jwt-auth'
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,13 +1,20 @@
 from .base import *
 DEBUG = False
+# SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE=True
+# SECURE_SSL_REDIRECT = True
+# SECURE_HSTS_PRELOAD=True
 # CORS_ORIGIN_WHITELIST = [
     
 # ]
 # CSRF_TRUSTED_ORIGINS = [
     
 # ]
-ALLOWED_HOSTS = ["https://stationery-rest-api.herokuapp.com",]
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ALLOWED_HOSTS = ["https://stationery-rest-api.herokuapp.com",]
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
