16ca02f5bd3757542a1ec34c4317560b4785db3a
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-23 16:03:57 +0900

    add security

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -80,7 +80,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE=True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_PRELOAD=True
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
