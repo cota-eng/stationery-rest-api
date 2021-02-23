d14394180ad02dc05ada5b7c59ab01f07ca78feb
cota <65804288+cota-eng@users.noreply.github.com>
2021-02-23 19:14:03 +0900

    for heroku

--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.append('../../')
+import config
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
