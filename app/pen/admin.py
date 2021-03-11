56ef80102274b0393d14566bbe53a091df284b08
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-11 12:04:36 +0900

    fix:

--- app/pen/admin.py
+++ app/pen/admin.py
@@ -10,27 +10,34 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [
         ReviewInline
     ]
-    # exclude = ('name',)
-    # fields = ('name',)
     list_display = ('name', 'price_yen',)
     list_filter = ('price_yen',)
-    # change_list_templates = 'admin/pen/pen_change_list.html'
 
 admin.site.register(models.Product,ProductAdmin)
 
+class FavAdmin(admin.ModelAdmin):
+    list_display = ('fav_user', 'product',)
+    list_filter = ('is_favorite',)
+
+admin.site.register(models.Fav,FavAdmin)
+
+
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug',)
-    # list_filter = ('name',)
+
 admin.site.register(models.Tag,TagAdmin)
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'official_site_link',)
+
 admin.site.register(models.Brand,BrandAdmin)
     
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug',)
+    
 admin.site.register(models.Category,CategoryAdmin)
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('avarage_star',)
 
-admin.site.register(models.Fav)
-admin.site.register(models.Review)
+admin.site.register(models.Review,ReviewAdmin)
