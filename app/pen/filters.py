b7b13935063abdc3426244ce53492d613f23f8d9
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-19 10:42:52 +0900

    :+1: fav filter add

--- app/pen/filters.py
+++ app/pen/filters.py
@@ -2,6 +2,16 @@ from django_filters import rest_framework as filters
 from . import models
 
 
+class OwnFavFilter(filters.FilterSet):
+    """
+    
+    """
+    fav_user = filters.CharFilter(field_name="fav_user", lookup_expr='exact')
+    # product = filters.CharFilter(field_name='product',lookup_expr='exact')
+    class Meta:
+        model = models.Fav
+        fields = ('fav_user',)
+        
 class ProductOriginalFilter(filters.FilterSet):
     """
     price - less than equal
