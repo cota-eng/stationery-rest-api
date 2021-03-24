9e863fb801939acd512af39640ac642673530586
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-24 12:16:06 +0900

    :+1: user test

--- app/authentication/tests/test_user_create.py
+++ app/authentication/tests/test_user_create.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 class CustomUserTests(TestCase):
-
+    fixtures = ["fixtures/master/avatar.json"]
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
@@ -10,9 +10,8 @@ class CustomUserTests(TestCase):
         )
         self.assertEqual(user.username,"testuser")
         self.assertEqual(user.email,"testuser@example.com")
-        self.assertEqual(user.password, "testuserpass")
         self.assertFalse(user.is_superuser)
-        self.assertEqual(user.is_staff)
+        self.assertFalse(user.is_staff)
 
     def test_create_super_user(self):
         User = get_user_model()
@@ -21,6 +20,5 @@ class CustomUserTests(TestCase):
         )
         self.assertEqual(user.username,"testadmin")
         self.assertEqual(user.email,"testadmin@example.com")
-        self.assertEqual(user.password, "testadminpass")
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
