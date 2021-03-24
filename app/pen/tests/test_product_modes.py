2f15aaa5a100e5da6e4f4b821aef4ea0ca2f9cb8
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-24 15:51:57 +0900

    :+1: add a test

---
+++ app/pen/tests/test_product_modes.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from pen.models import (
+    Product,
+    Category,
+    Tag,
+    Brand,
+)
+
+"""
+各メソッド内で使うユーザー作成、カテゴリ、タグ作成
+
+"""
+
+class ProductModelTests(TestCase):
+    
+    def setUp(self):
+
+        self.category = Category.objects.create(
+            name="鉛筆",
+            slug="pencil",
+        )
+
+        self.brand = Brand.objects.create(
+            name="アイウ工業",
+            slug="aiu-factory",
+            official_site_link="aiu.example.com"
+        )
+
+        self.tag = Tag.objects.create(
+            name="安価",
+            slug="low-price", 
+        )
+
+    def test_create_product_success(self):
+        product1 = Product.objects.create(
+            name="あいう鉛筆",
+            description="",
+            price_yen=150,
+            image="products/default.jpg",
+            image_src="https://example.com",
+            amazon_link_to_buy="",
+            rakuten_link_to_buy="",
+            category=self.category,
+            brand=self.brand,
+        )
+        product1.tag.set([self.tag])
+        self.assertTrue(Product.objects.all().exists())
