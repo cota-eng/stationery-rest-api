ff137b57920473339568acde82a9b0516ba4811c
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-30 22:09:11 +0900

    :hammer: create post model

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField()
+    content = models.TextField()
+    is_public = models.BooleanField(default=False)
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name="author",
+        on_delete=models.CASCADE
+        )
+    published_at = models.DateTimeField(auto_now_add=True)
+    
+
+class Comment(models.Model):
+    pass
