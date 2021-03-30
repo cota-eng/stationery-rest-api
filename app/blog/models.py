33da4d206282fd74e5241b5cb69d0dcf5995f49f
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-31 08:53:52 +0900

    :+1: add markdowm

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from markdown import markdown,mark_safe
 class Post(models.Model):
     title = models.CharField(max_length=30)
     slug = models.SlugField()
@@ -12,7 +12,24 @@ class Post(models.Model):
         on_delete=models.CASCADE
         )
     published_at = models.DateTimeField(auto_now_add=True)
+
     
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
+
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
 
 class Comment(models.Model):
-    pass
+    content = models.TextField()
+    commentator = models.ForeignKey(
+        get_user_model(),
+        related_name="commentator",
+        on_delete=models.CASCADE
+        )
+    # parent = 
