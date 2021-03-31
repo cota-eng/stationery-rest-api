883124ee8d03cd23a45e16bc9dec1a4f9db30943
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-31 13:28:26 +0900

    :fire: add serialzeir

--- app/blog/serializers.py
+++ app/blog/serializers.py
@@ -10,14 +10,37 @@ class PostSerializer(ModelSerializer):
         lookup_field="",
     )
     markdown = SerializerMethodField()
+    comments = SerializerMethodField()
 
     def get_markdown(self, obj):
         return obj.get_markdown()
 
+    def get_comments(self, obj):
+        content_type = obj.get_content_type
+        object_id = obj.id
+        comment_qs = models.Comment.objects.filter_by_instance(obj)
+        comments = CommentSerializer(data=comment_qs, many=True).data
+        return comments
+
+def create_comment_serializer(type="post", slug=None, parent_id=None):
+    class CommentCreateSeralizer(ModelSerializer):
+        class Meta:
+            model = models.Comment
+            fields = (
+                'id',
+                'parent',
+                'content',
+            )
+
 class CommentSerializer(ModelSerializer):
+    reply_count = SerializerMethodField()
     class Meta:
         model = models.Comment
         fields = "__all__"
+    def get_reply_count(self, obj)->int:
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
 
 
 class CommentChildSerializer(ModelSerializer):
