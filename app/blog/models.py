168d0b58cdf8d93db6e2a9afb71da09ce493282e
cota <65804288+cota-eng@users.noreply.github.com>
2021-03-31 19:30:04 +0900

    :fire: add function in manager

--- app/blog/models.py
+++ app/blog/models.py
@@ -5,6 +5,8 @@ from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation  
 from django.contrib.contenttypes.models import ContentType
+
+
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
         return super(PostManager,self).filter(is_public=True).filter(pub)
@@ -60,12 +62,35 @@ def pre_save_post_reciewver(sender, instance, *args, **kwargs):
         instance.slug = create_slug(instance)
 
 class CommentManager(models.Manager):
+
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
         qs = super(CommentManager,self).filter(content_type=content_type,object_id=obj_id)
         return qs
 
+    def create_by_model_type(self, model_type, slug, content, user, parent_obj):
+        model_qs = ContentType.objects.filter(model=model_type)
+        if model_qs.exists():
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(slug=self.slug)
+            if obj_qs.exists() and obj_qs.count() == 1:
+                instance = self.model()
+                instance.content = content
+                instance.user = user
+                instance.content_type = model_qs.first()
+                instance.object_id = obj_qs.first().id
+                if parent_obj:
+                    instance.parent = parent_obj
+                instance.save()
+                return instance
+        return None
+
+    
 
 class Comment(models.Model):
     content = models.TextField()
