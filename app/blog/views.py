2f4ab967f54c490797afa4c8d4dec5a94ae5f4f4
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-01 13:40:18 +0900

    :memo: add some views

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import (
+    PostListSerializer,
+)
+from .models import (
+    Post,
+    Comment,
+)
 
-# Create your views here.
+
+
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = PostListSerializer
+    queryset = Post.objects.all()
+
+class PostDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = PostListSerializer
+    queryset = Post.objects.all()
+    lookup_field = "slug"
+
+class PostCreateAPIView(generics.CreateAPIView):
+    serializer_class = PostListSerializer
+    queryset = Post.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        return super().perform_create(serializer)
