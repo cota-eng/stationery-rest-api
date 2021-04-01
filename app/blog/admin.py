0d51214c2a387a788b86f6fc0228592ba4c2852e
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-01 14:07:08 +0900

    :fire: custom admin

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["titie", "updated_at"]
+    list_display_links = ["updated_at"]
+    list_editable = ["title"]
+    list_filter = ["updated_at"]
+    search_fields = ["title", "content"]
+    
+    class Meta:
+        model = Post
+        
+admin.site.register(Post,PostModelAdmin)
+    
