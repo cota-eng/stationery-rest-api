b62f5664ecde78b30f920b5b294711a2b65dc846
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-06 09:10:48 +0900

    Revert "Feature/sql"

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from rest_framework import views
 from rest_framework import viewsets
 from rest_framework_simplejwt.tokens import RefreshToken
-from . import models
-from . import serializers
+from . import models,serializers
 from django.conf import settings
+# from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from rest_framework import exceptions
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+# from django.contrib.auth.tokens import PasswordResetTokenGenerator
+# from django.shortcuts import redirect
+# from django.http import HttpResponsePermanentRedirect
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
@@ -22,10 +25,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import parsers
 from rest_framework import mixins
 from rest_framework import generics
-# from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
-# from django.contrib.auth.tokens import PasswordResetTokenGenerator
-# from django.shortcuts import redirect
-# from django.http import HttpResponsePermanentRedirect
 
 """
 4 views needed
@@ -35,11 +34,18 @@ from rest_framework import generics
 - logout
 """
 
+# class LoginAPIView(generics.GenericAPIView):
+#     authentication_classes = [] # disable authentication
+#     permission_classes = (permissions.AllowAny,)
+#     serializer_class = serializers.LoginSerializer
+#     def post(self, request):
+#         serialier = self.serializer_class(data=request.data)
+#         serialier.is_valid(raise_exception=True)
+
+#         return Response(serialier.data,status=status.HTTP_200_OK)
+
 class GoogleLogin(SocialLoginView):
-    """
-    For google oauth
-    """
-    authentication_classes = [] 
+    authentication_classes = [] # disable authentication
     adapter_class = GoogleOAuth2Adapter
     callback_url = "http://localhost:3000"
     client_class = OAuth2Client
@@ -51,21 +57,14 @@ class UserReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     変更可能な情報はprofileにのみ存在してるから、、、
     UserRankingなどに必要かも
     """
-    # TODO: serializer - 
     queryset = get_user_model().objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = (permissions.AllowAny,)
     lookup_field = "id"
 
-    def get_queryset(self):
-        qs = self.queryset
-        qs = self.get_serializer_class().setup_for_query(qs)
-        return qs
-
 class WhoAmIView(
     mixins.ListModelMixin,
     viewsets.GenericViewSet):
-    # TODO: serializer - ok
     queryset = models.Profile.objects.all()
     # permission_classes = (permissions.AllowAny,)
     permission_classes = (permissions.IsAuthenticated,)
@@ -77,17 +76,13 @@ class WhoAmIView(
 class ProfileReadOnlyViewSet(mixins.RetrieveModelMixin,
                              mixins.ListModelMixin,
                              viewsets.GenericViewSet):
-    queryset = models.Profile.objects.all().prefetch_related("user__reviewer")
+    queryset = models.Profile.objects.all()
     # permission_classes = (permissions.AllowAny,)
     permission_classes = (permissions.AllowAny,)
     serializer_class = serializers.ProfileSerializer
-    #TODO serializer Recreate
-    # parser_classes = [parsers.MultiPartParser,
-    #                   parsers.FormParser,]
-    def get_queryset(self):
-        qs = self.queryset
-        qs = self.get_serializer_class().setup_for_query(qs)
-        return qs
+    parser_classes = [parsers.MultiPartParser,
+                      parsers.FormParser,]
+
 
 class OwnProfileListRetrieveUpdateViewSet(mixins.RetrieveModelMixin,
                                           mixins.ListModelMixin,
@@ -96,7 +91,7 @@ class OwnProfileListRetrieveUpdateViewSet(mixins.RetrieveModelMixin,
     """
     for your own profile
     """
-    queryset = models.Profile.objects.all().select_related("avatar")
+    queryset = models.Profile.objects.all()
     # permission_classes = (permissions.AllowAny,)
     permission_classes = (permissions.IsAuthenticated,
                           UserIsOwnerOrReadOnly,
@@ -108,25 +103,6 @@ class OwnProfileListRetrieveUpdateViewSet(mixins.RetrieveModelMixin,
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
 
-class LogoutView(GenericAPIView):
-    serializer_class = LogoutSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def post(self, request, *args):
-        sz = self.get_serializer(data=request.data)
-        sz.is_valid(raise_exception=True)
-        sz.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class LoginAPIView(generics.GenericAPIView):
-#     authentication_classes = [] # disable authentication
-#     permission_classes = (permissions.AllowAny,)
-#     serializer_class = serializers.LoginSerializer
-#     def post(self, request):
-#         serialier = self.serializer_class(data=request.data)
-#         serialier.is_valid(raise_exception=True)
-
-#         return Response(serialier.data,status=status.HTTP_200_OK)
 
 # class AvatarRetrieveUpdateView( mixins.RetrieveModelMixin,
 #                         mixins.ListModelMixin,
@@ -168,3 +144,13 @@ class LogoutView(GenericAPIView):
     #     response = {'message': 'post method is not allowed'}
     #     return Response(response, status=status.HTTP_400_BAD_REQUEST)
    
+
+class LogoutView(GenericAPIView):
+    serializer_class = LogoutSerializer
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, *args):
+        sz = self.get_serializer(data=request.data)
+        sz.is_valid(raise_exception=True)
+        sz.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- app/pen/urls.py
+++ app/pen/urls.py
@@ -6,25 +6,18 @@ router = routers.DefaultRouter()
 
 router.register('category',views.CategoryReadOnlyViewSet)
 router.register('brand',views.BrandReadOnlyViewSet)
-router.register('tag', views.TagReadOnlyViewSet)
-"""
-below 3 view is not needed
-"""
+router.register('tag',views.TagReadOnlyViewSet)
 router.register('product-category',views.ProductCategoryFilteredReadOnlyViewSet)
 router.register('product-brand',views.ProductBrandFilteredReadOnlyViewSet)
-router.register('product-tag', views.ProductTagFilteredReadOnlyViewSet)
-# so many query
+router.register('product-tag',views.ProductTagFilteredReadOnlyViewSet)
 router.register('search',views.ProductSearchByAllConditions)
 router.register('fav',views.FavProductAPIView)
-# router.register('products',views.ProductRetrieveView)
+router.register('products',views.ProductReadOnlyViewSet)
 router.register('pro',views.ProductPagingReadOnlyViewSet)
 router.register('review',views.ReviewViewSet)
 # router.register('review-read',views.ReviewReadOnlyViewSet)
 router.register('my-review',views.OwnReviewViewSet)
 app_name="pen"
 urlpatterns = [
-    path("products/",views.ProductListAPIView.as_view(),name="product-list"),
-    path("products/<str:pk>/",views.ProductRetrieveAPIView.as_view(),name="product"),
-    path("fav-list/",views.ReturnFavProductAPIView.as_view(),name="fav-list"),
-    path('',include(router.urls)),
+    path('',include(router.urls))
 ]
