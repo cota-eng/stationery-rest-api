a4eec38776a6bc7351403c27f7298f55eee67f7d
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-11 08:41:58 +0900

    add fav permission

--- app/pen/permissions.py
+++ app/pen/permissions.py
@@ -12,3 +12,16 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         # Instance must have an attribute named `owner`.
         return obj.owner == request.user
+
+class IsFavUserOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `fav-user` attribute.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
+        # Instance must have an attribute named `owner`.
+        return obj.fav_user == request.user
