ceb7a3b2bd7f707791f3f0f3ac6f19d9ba67f2b3
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-10 15:04:27 +0900

    add

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -63,10 +63,9 @@ DATABASES['default'].update(db_from_env)
 """
 static
 """
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
