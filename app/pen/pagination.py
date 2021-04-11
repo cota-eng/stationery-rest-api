e8c3e3dce848bf60a95cdd44c9dc90c6a954ec7a
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-11 15:47:22 +0900

    :fire: change size

--- app/pen/pagination.py
+++ app/pen/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 class NormalPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 12
     # page_size_query_param = 'page_size'
     # max_page_size = 10000
 
