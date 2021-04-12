f0bdf4d4b5d4ba719b70dfec8a0c86065f4356a5
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-12 22:32:39 +0900

    :memo: fix command

--- app/core/management/commands/set_related_products.py
+++ app/core/management/commands/set_related_products.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for product in Product.objects.all():
             related = Product.objects.exclude(pk=product.pk).filter(Q(related_products__brand__pk=product.brand.pk) & Q(related_products__category__pk=product.category.pk)).order_by("?")[:6]
-            sleep(1)
+            sleep(0.5)
             # | Q(related_products__tag__pk=product.tag.pk)
             # product.related_products
             # Product.objects.create(product=product,related_products=related)
-            # product.related_products.set(related)
+            product.related_products.set(related)
             # pprint(product.related_products.all())
-            # pprint(product)
+            pprint(product)
             # pprint(related)
