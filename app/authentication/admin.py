1a7cb0e375cf7ed6b716f7da86141cc6386e444e
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-14 15:13:57 +0900

    delete avatar

--- app/authentication/admin.py
+++ app/authentication/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import Profile, User, Avatar
+from .models import Profile, User
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','created_at','updated_at',)
+    list_display = ('user','created_at',)
     # list_filter = ('name',)
 
 
-class AvatarAdmin(admin.ModelAdmin):
-    list_display = ('name','image')
+# class AvatarAdmin(admin.ModelAdmin):
+    # list_display = ('name','image')
 
 admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Avatar,AvatarAdmin)
+# admin.site.register(Avatar,AvatarAdmin)
