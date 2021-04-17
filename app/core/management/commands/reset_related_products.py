eb1691c5d0d6fafe6e6d4e547df07f98de454435
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-17 15:47:05 +0900

    webhook

--- app/core/management/commands/reset_related_products.py
+++ app/core/management/commands/reset_related_products.py
@@ -4,8 +4,16 @@ from pen.models import Product
 from django.db.models import Q
 from pprint import pprint
 from time import sleep
+import requests
+
+env = environ.Env()
+env.read_env('SLACK_WEBHOOK_NEWS')
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for product in Product.objects.all():
             product.related_products.clear()
             sleep(0.1)
+        WEB_HOOK_URL = env.get_value("SLACK_WEBHOOK_NEWS")
+        requests.post(WEB_HOOK_URL, data = {
+            'text': "fin! set related products" })
