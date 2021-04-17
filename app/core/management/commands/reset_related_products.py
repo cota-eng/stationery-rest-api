207414d5d8f0380a2e6537f20300f86a6018baeb
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-17 12:39:13 +0900

    :+1: reset batch

---
+++ app/core/management/commands/reset_related_products.py
@@ -0,0 +1,15 @@
+import logging
+from django.core.management.base import BaseCommand, CommandError
+from pen.models import Product
+from django.db.models import Q
+from pprint import pprint
+from time import sleep
+class Command(BaseCommand):
+    """
+    
+    """
+
+    def handle(self, *args, **options):
+        for product in Product.objects.all():
+            product.related_products.clear()
+            sleep(0.1)
