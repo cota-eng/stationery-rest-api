c1d131801ee927bb2efdc409ae92c3e1edee32e8
cota <65804288+cota-eng@users.noreply.github.com>
2021-04-17 13:29:58 +0900

    :bug: fixed

--- app/core/management/commands/set_related_products.py
+++ app/core/management/commands/set_related_products.py
@@ -6,15 +6,11 @@ from pprint import pprint
 from time import sleep
 class Command(BaseCommand):
     """
-    
     """
 
     def handle(self, *args, **options):
         for product in Product.objects.all():
-            related = Product.objects.exclude(pk=str(product.id)).filter(
-                Q(related_products__brand__pk=product.brand.id) |
-                Q(related_products__category__pk=product.category.id)
-                ).order_by("?").distinct()[:6]
+            related = Product.objects.filter(Q(brand=product.brand.id) | Q(category=product.category.id)).exclude(id=str(product.id)).order_by("?").distinct()[:6]
             product.related_products.set(related)
             # pprint(related)
             # pprint(str(product.pk))
